541a3c21ccc15aac90f7f149d1a327bf31223656
AwebDevOmar <114593549+AwebDevOmar@users.noreply.github.com>
2022-09-28 22:59:44 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
-var yourName=document.querySelector(".yourName"),crushName=document.querySelector(".crushName"),btn=document.querySelector(".btn"),gif=document.querySelector(".gif"),gifContainer=document.querySelector(".gifContainer"),result=document.querySelector(".result"),resultContainer=document.querySelector(".resultContainer"),match=Math.floor(100*Math.random()+1);btn.addEventListener("click",function(){yourName=yourName.value,crushName=crushName.value;var i=yourName.length,e=crushName.length;i>2&&e>2?match<=30?(gifContainer.classList.remove("visibility"),gif.src="images/10up.gif",resultContainer.classList.remove("visibility"),result.innerHTML="Your Match is "+match+"%"):match>30&&match<=50?(gifContainer.classList.remove("visibility"),gif.src="images/30up.gif",resultContainer.classList.remove("visibility"),result.innerHTML="Your Match is "+match+"%"):match>50&&match<=80?(gifContainer.classList.remove("visibility"),gif.src="images/50up.gif",resultContainer.classList.remove("visibility"),result.innerHTML="Your Match is "+match+"%"):match>80&&match<=90?(gifContainer.classList.remove("visibility"),gif.src="images/80up.gif",resultContainer.classList.remove("visibility"),result.innerHTML="Your Match is "+match+"%"):(gifContainer.classList.remove("visibility"),gif.src="images/90up.gif",resultContainer.classList.remove("visibility"),result.innerHTML="Your Match is "+match+"%"):(resultContainer.classList.remove("visibility"),result.innerHTML="Please Enter Correct Name")});
+var yourName = document.querySelector(".yourName");
+var crushName = document.querySelector(".crushName");
+var btn = document.querySelector(".btn");
+var gif = document.querySelector(".gif");
+var gifContainer =document.querySelector(".gifContainer");
+var result = document.querySelector(".result");
+var resultContainer = document.querySelector(".resultContainer");
+
+var match = Math.floor(Math.random()*100 + 1);
+
+btn.addEventListener("click", function(){
+    yourName = yourName.value ;
+    crushName = crushName.value ;
+
+    var a = yourName.length ;
+    var b = crushName.length ;
+     
+ if (a>2 && b>2){
+
+    if (match<=30){
+        gifContainer.classList.remove("visibility");
+        gif.src=("images/10up.gif");
+        resultContainer.classList.remove("visibility");
+        result.innerHTML="Your Match is "+match+"%" ;
+        
+    }
+    else if(yourName==crushName){
+
+        gifContainer.classList.remove("visibility");
+        gif.src=("images/gay.gif");
+        resultContainer.classList.remove("visibility");
+        result.innerHTML="No Homo Plz" ;
+
+    }
+    
+    else if(match>30 && match<=50){
+
+        gifContainer.classList.remove("visibility");
+        gif.src=("images/30up.gif");
+        resultContainer.classList.remove("visibility");
+        result.innerHTML="Your Match is "+match+"%" ;
+
+    }
+    else if(match>50 && match<=80){
+        gifContainer.classList.remove("visibility");
+        gif.src=("images/50up.gif");
+        resultContainer.classList.remove("visibility");
+        result.innerHTML="Your Match is "+match+"%" ;
+
+    }
+    else if(match>80 && match<=90){
+        gifContainer.classList.remove("visibility");
+        gif.src=("images/80up.gif");
+        resultContainer.classList.remove("visibility");
+        result.innerHTML="Your Match is "+match+"%" ;
+
+    }
+    else {
+        gifContainer.classList.remove("visibility");
+        gif.src=("images/90up.gif");
+        resultContainer.classList.remove("visibility");
+        result.innerHTML="Your Match is "+match+"%" ;
+
+    }
+
+   
+}
+else {
+    resultContainer.classList.remove("visibility");
+        result.innerHTML="Please Enter Correct Name" ;
+
+}
+}
+);
+
+
+
